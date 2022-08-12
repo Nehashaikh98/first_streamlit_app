3e6f78f2c2b043a870de687b6976e272b40e56fc
Nehashaikh98 <110591008+Nehashaikh98@users.noreply.github.com>
2022-08-12 11:42:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ streamlit.title('My parents run a new healthy diner')
 
 streamlit.header('omega 3 and green vegetables are on the menu')
 streamlit.text('They are open Mon to Fri 10am to 9pm')
+streamlit.text('The menu is exciting healthy and tasty')
+streamlit.text('Pastries and Cake')
+streamlit.text('Sandwiches and Fries')
+streamlit.text('Pizzas and Burgers')
